8625fa7067c57a2def4d7d7c04139ed906400572
KimHeng2022 <soloqt9@gmail.com>
2022-12-02 17:47:45 +0700

    lol

---
+++ Task/Test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Please input ur gender ( M or F ) = ");
+    scanf("%d",a);
+
+    if(a="M"){
+        printf("Hello Mr");
+    }
+    else if(a="F"){
+        printf("Hello Mrs");
+    }
+}
